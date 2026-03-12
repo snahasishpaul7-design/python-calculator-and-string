47f30a90222d2794968f45799be63cba1e7c87b7
Tonmoy pal <snahasishpaul7@gmail.com>
2026-03-12 13:46:28 +0600

    just quiz code

---
+++ quiz.py
@@ -0,0 +1,604 @@
+print("Welcome Player Number 001!")
+
+play = input("Do You Want Play A Quiz Game? " )
+
+if play.lower() != "yes":
+    print(quit(" Ok See You Again Loser:("))
+else:
+    print("Ok Let's Play Like A Hero! :) And Each And Every Word You Use For Quiz Is Should Be Capital ")
+score = 0
+a= print("What is the capital of France? ")
+print("A = PARIS")
+print("B = ROME")
+Answer = input("WRITE YOUR ANSWER:-" )
+
+if Answer.upper()== "PARIS": 
+    
+    print("CORRECT,YOU GET ONE STAR")
+    score += 1 #increment of score
+    
+else:
+    print("Incorrect,YOU ARE A LOSER")    
+b = print("Which planet is known as the Red Planet? ")
+print("A = VENUS")
+print("B = MARS")
+Answer_1 =input("WRITE YOUR ANSWER:-")
+
+if Answer_1.upper()== "MARS":
+    print("CORRECT,YOU GET TWO STAR. GREAT!")
+    score+=1 #increment of score
+else:
+    print("Incorrect")
+print("Who invented the light bulb?")
+print("A = EDISON")
+print("B = NEWTON")
+Answer_4 = input("WRITE YOUR ANSWER:- ")
+if Answer_4.upper() == "EDISON":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+print("Which is the longest river?")
+print("A = AMAZON")
+print("B = NILE")
+Answer_5 = input("WRITE YOUR ANSWER:- ")
+if Answer_5.upper() == "AMAZON":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which animal is known as King of Jungle?")
+print("A = TIGER")
+print("B = LION")
+Answer_6 = input("WRITE YOUR ANSWER:- ")
+if Answer_6.upper() == "LION":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the freezing point of water?")
+print("A = 0")
+print("B = 100")
+Answer_7 = input("WRITE YOUR ANSWER:- ")
+if Answer_7 == "0":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which gas do plants use for photosynthesis?")
+print("A = CO2")
+print("B = OXYGEN")
+Answer_8 = input("WRITE YOUR ANSWER:- ")
+if Answer_8.upper() == "CO2":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which planet is closest to the sun?")
+print("A = MERCURY")
+print("B = VENUS")
+Answer_9 = input("WRITE YOUR ANSWER:- ")
+if Answer_9.upper() == "MERCURY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the chemical symbol for water?")
+print("A = H2O")
+print("B = CO2")
+Answer_10 = input("WRITE YOUR ANSWER:- ")
+if Answer_10.upper() == "H2O":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which country has the most population?")
+print("A = INDIA")
+print("B = CHINA")
+Answer_11 = input("WRITE YOUR ANSWER:- ")
+if Answer_11.upper() == "INDIA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which color is a mix of red and blue?")
+print("A = PURPLE")
+print("B = ORANGE")
+Answer_12 = input("WRITE YOUR ANSWER:- ")
+if Answer_12.upper() == "PURPLE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which continent is Egypt in?")
+print("A = AFRICA")
+print("B = ASIA")
+Answer_13 = input("WRITE YOUR ANSWER:- ")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+if Answer_13.upper() == "AFRICA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which is the largest desert?")
+print("A = SAHARA")
+print("B = ARABIAN")
+Answer_14 = input("WRITE YOUR ANSWER:- ")
+if Answer_14.upper() == "SAHARA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which fruit has seeds on its outside?")
+print("A = STRAWBERRY")
+print("B = BANANA")
+Answer_15 = input("WRITE YOUR ANSWER:- ")
+if Answer_15.upper() == "STRAWBERRY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Who discovered gravity?")
+print("A = NEWTON")
+print("B = GALILEO")
+Answer_16 = input("WRITE YOUR ANSWER:- ")
+if Answer_16.upper() == "NEWTON":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the capital of Japan?")
+print("A = TOKYO")
+print("B = BEIJING")
+Answer_17 = input("WRITE YOUR ANSWER:- ")
+if Answer_17.upper() == "TOKYO":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What do bees produce?")
+print("A = HONEY")
+print("B = MILK")
+Answer_18 = input("WRITE YOUR ANSWER:- ")
+if Answer_18.upper() == "HONEY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which is the tallest mountain?")
+print("A = K2")
+print("B = EVEREST")
+Answer_19 = input("WRITE YOUR ANSWER:- ")
+if Answer_19.upper() == "EVEREST":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which planet is famous for rings?")
+print("A = JUPITER")
+print("B = SATURN")
+Answer_20 = input("WRITE YOUR ANSWER:- ")
+if Answer_20.upper() == "SATURN":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which is the fastest land animal?")
+print("A = CHEETAH")
+print("B = LION")
+Answer_21 = input("WRITE YOUR ANSWER:- ")
+if Answer_21.upper() == "CHEETAH":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which part of the plant makes food?")
+print("A = ROOT")
+print("B = LEAF")
+Answer_22 = input("WRITE YOUR ANSWER:- ")
+if Answer_22.upper() == "LEAF":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which instrument is used to see small things?")
+print("A = MICROSCOPE")
+print("B = TELESCOPE")
+Answer_23 = input("WRITE YOUR ANSWER:- ")
+if Answer_23.upper() == "MICROSCOPE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("How many continents are there?")
+print("A = 7")
+print("B = 5")
+Answer_24 = input("WRITE YOUR ANSWER:- ")
+if Answer_24 == "7":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which metal is liquid at room temperature?")
+print("A = MERCURY")
+print("B = IRON")
+Answer_25 = input("WRITE YOUR ANSWER:- ")
+if Answer_25.upper() == "MERCURY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which organ pumps blood?")
+print("A = LUNGS")
+print("B = HEART")
+Answer_26 = input("WRITE YOUR ANSWER:- ")
+if Answer_26.upper() == "HEART":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("How many bones in the human body?")
+print("A = 206")
+print("B = 210")
+Answer_27 = input("WRITE YOUR ANSWER:- ")
+if Answer_27 == "206":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which animal has a trunk?")
+print("A = ELEPHANT")
+print("B = GIRAFFE")
+Answer_28 = input("WRITE YOUR ANSWER:- ")
+if Answer_28.upper() == "ELEPHANT":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the main gas we breathe?")
+print("A = OXYGEN")
+print("B = NITROGEN")
+Answer_29 = input("WRITE YOUR ANSWER:- ")
+if Answer_29.upper() == "OXYGEN":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which day comes after Friday?")
+print("A = SUNDAY")
+print("B = SATURDAY")
+Answer_30 = input("WRITE YOUR ANSWER:- ")
+if Answer_30.upper() == "SATURDAY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which shape has three sides?")
+print("A = TRIANGLE")
+print("B = RECTANGLE")
+Answer_31 = input("WRITE YOUR ANSWER:- ")
+if Answer_31.upper() == "TRIANGLE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the capital of Bangladesh?")
+print("A = DHAKA")
+print("B = CHITTAGONG")
+Answer_32 = input("WRITE YOUR ANSWER:- ")
+if Answer_32.upper() == "DHAKA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which is the smallest planet?")
+print("A = MARS")
+print("B = MERCURY")
+Answer_33 = input("WRITE YOUR ANSWER:- ")
+if Answer_33.upper() == "MERCURY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What does H2O stand for?")
+print("A = WATER")
+print("B = SALT")
+Answer_34 = input("WRITE YOUR ANSWER:- ")
+if Answer_34.upper() == "WATER":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which bird cannot fly?")
+print("A = PENGUIN")
+print("B = CROW")
+Answer_35 = input("WRITE YOUR ANSWER:- ")
+if Answer_35.upper() == "PENGUIN":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is used to write on blackboards?")
+print("A = CHALK")
+print("B = PEN")
+Answer_36 = input("WRITE YOUR ANSWER:- ")
+if Answer_36.upper() == "CHALK":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which food is made from milk?")
+print("A = CHEESE")
+print("B = BREAD")
+Answer_37 = input("WRITE YOUR ANSWER:- ")
+if Answer_37.upper() == "CHEESE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which planet has a big storm called the Great Red Spot?")
+print("A = JUPITER")
+print("B = MARS")
+Answer_38 = input("WRITE YOUR ANSWER:- ")
+if Answer_38.upper() == "JUPITER":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("How many legs does a spider have?")
+print("A = 6")
+print("B = 8")
+Answer_39 = input("WRITE YOUR ANSWER:- ")
+if Answer_39 == "8":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What color is the sky?")
+print("A = BLUE")
+print("B = GREEN")
+Answer_40 = input("WRITE YOUR ANSWER:- ")
+if Answer_40.upper() == "BLUE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("How many hours are in a day?")
+print("A = 24")
+print("B = 12")
+Answer_41 = input("WRITE YOUR ANSWER:- ")
+if Answer_41 == "24":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which gas is used in balloons?")
+print("A = HELIUM")
+print("B = HYDROGEN")
+Answer_42 = input("WRITE YOUR ANSWER:- ")
+if Answer_42.upper() == "HELIUM":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which part of the body helps you to see?")
+print("A = EYES")
+print("B = EARS")
+Answer_43 = input("WRITE YOUR ANSWER:- ")
+if Answer_43.upper() == "EYES":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which shape has four equal sides?")
+print("A = SQUARE")
+print("B = TRIANGLE")
+Answer_44 = input("WRITE YOUR ANSWER:- ")
+if Answer_44.upper() == "SQUARE":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What do you use to hear music?")
+print("A = EARS")
+print("B = EYES")
+Answer_45 = input("WRITE YOUR ANSWER:- ")
+if Answer_45.upper() == "EARS":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which animal gives us wool?")
+print("A = SHEEP")
+print("B = COW")
+Answer_46 = input("WRITE YOUR ANSWER:- ")
+if Answer_46.upper() == "SHEEP":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which planet is known for having life?")
+print("A = EARTH")
+print("B = VENUS")
+Answer_47 = input("WRITE YOUR ANSWER:- ")
+if Answer_47.upper() == "EARTH":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which season is the coldest?")
+print("A = WINTER")
+print("B = SUMMER")
+Answer_48 = input("WRITE YOUR ANSWER:- ")
+if Answer_48.upper() == "WINTER":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which month has 28 or 29 days?")
+print("A = FEBRUARY")
+print("B = MARCH")
+Answer_49 = input("WRITE YOUR ANSWER:- ")
+if Answer_49.upper() == "FEBRUARY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the opposite of 'cold'?")
+print("A = HOT")
+print("B = COOL")
+Answer_50 = input("WRITE YOUR ANSWER:- ")
+if Answer_50.upper() == "HOT":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the national flower of Bangladesh?")
+print("A = Water Lily")
+print("B = Rose")
+Answer_51 = input("WRITE YOUR ANSWER:- ")
+if Answer_51.upper() == "WATER LILY":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the capital city of Bangladesh?")
+print("A = Dhaka")
+print("B = Chittagong")
+Answer_52 = input("WRITE YOUR ANSWER:- ")
+if Answer_52.upper() == "DHAKA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which river is known as the 'Old Ganges' in Bangladesh?")
+print("A = Padma")
+print("B = Jamuna")
+Answer_53 = input("WRITE YOUR ANSWER:- ")
+if Answer_53.upper() == "PADMA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Who is known as the 'Father of the Nation' in Bangladesh?")
+print("A = Sheikh Mujibur Rahman")
+print("B = Ziaur Rahman")
+Answer_54 = input("WRITE YOUR ANSWER:- ")
+if Answer_54.upper() == "SHEIKH MUJIBUR RAHMAN":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("In which year did Bangladesh gain independence?")
+print("A = 1971")
+print("B = 1947")
+Answer_55 = input("WRITE YOUR ANSWER:- ")
+if Answer_55 == "1971":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the currency of Bangladesh?")
+print("A = Taka")
+print("B = Rupee")
+Answer_56 = input("WRITE YOUR ANSWER:- ")
+if Answer_56.upper() == "TAKA":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which sea borders Bangladesh to the south?")
+print("A = Bay of Bengal")
+print("B = Arabian Sea")
+Answer_57 = input("WRITE YOUR ANSWER:- ")
+if Answer_57.upper() == "BAY OF BENGAL":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("What is the national animal of Bangladesh?")
+print("A = Royal Bengal Tiger")
+print("B = Elephant")
+Answer_58 = input("WRITE YOUR ANSWER:- ")
+if Answer_58.upper() == "ROYAL BENGAL TIGER":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which language is officially spoken in Bangladesh?")
+print("A = Bengali")
+print("B = Hindi")
+Answer_59 = input("WRITE YOUR ANSWER:- ")
+if Answer_59.upper() == "BENGALI":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("Which is the largest island in Bangladesh?")
+print("A = Bhola Island")
+print("B = Saint Martin Island")
+Answer_60 = input("WRITE YOUR ANSWER:- ")
+if Answer_60.upper() == "BHOLA ISLAND":
+    print("CORRECT!")
+    score += 1
+else:
+    print("Incorrect")
+
+
+    
+print ("YOUR SCORE IS"" "+     str(score)   +  " " "OUT OF 60)")
+print("YOU GOT" " "  +  str((score/60)*100) + "%")
